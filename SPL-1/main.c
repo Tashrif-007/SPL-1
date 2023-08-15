51661e19239b1c9d078900724cfa7f3d4823a52e
Tashrif2001 <bsse1448@iit.du.ac.bd>
2023-08-16 01:56:53 +0600

    read file in byte mode into state array

--- SPL-1/main.c
+++ SPL-1/main.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void pad_bytes(unsigned char byteStream[], size_t *len)
+{
+    size_t padLen = 16-(*len % 16);
+    for(size_t i=0; i<padLen; i++)
+    {
+        byteStream[*len +i] = (unsigned char)padLen;
+    }
+    *len+=padLen;
+}
+
+void stateArray(const unsigned char *byteStream, unsigned char state[4][4])
+{
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            int idx = i*4+j;
+            state[j][i]=byteStream[idx];
+        }
+    }
+}
+
 int main()
 {
+    FILE *file = fopen("input.txt", "rb");
+    if(file==NULL)
+    {
+        printf("Error reading file");
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    unsigned char byteStream[256];
+    size_t len=0;
+
+    while(len<size)
+    {
+        size_t read = fread(byteStream+len, 1, 16, file);
+        len+=read;
+    }
+    fclose(file);
+
+    pad_bytes(byteStream, &len);
+
+    unsigned char state[4][4];
+    stateArray(byteStream, state);
+
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        printf("%02x ", state[i][j]);
+    }
 
     return 0;
 }
